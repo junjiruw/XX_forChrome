55d9084ad67fba64c10aedc98e28ba1721b2f687
junjiruw <junjiruw@yahoo.co.jp>
2023-09-10 13:29:43 +0900

    DM表示に失敗したとき用のボタンを作ったよ

--- XX.js
+++ XX.js
@@ -1,25 +1,62 @@
 let キー入力の許可=true;
-let DMすでに出てる=false;
+
+function DM出現判定(){
+    let DM親 = document.getElementById("layers").firstElementChild.firstElementChild.firstElementChild.firstElementChild;
+    return DM親.classList.contains("r-5wli1b");
+    //DMの頭が出てればこいつがTRUE。それすらないとFALSE。あと、そもそもページが細すぎると要素がないのでこれもFALSEだが、知らん！
+    //ただ、この判定法は万能ではないっぽい（出ててもFALSEのことがある。知らん！）
+}
+
 window.addEventListener('load', function(){
 
     //ロード終わったら2秒後に操作するように予約しておく
     setTimeout(function(){
         //DMの表示を自動で行う
-        let DM親 = document.getElementById("layers").firstElementChild.firstElementChild.firstElementChild.firstElementChild;
-        
-        DMすでに出てる=DM親.classList.contains("r-5wli1b");//DMの頭が出てればこいつがTRUE。それすらないとFALSE。あと、そもそもページが細すぎると要素がないのでこれもFALSEだが、知らん！
+        //まあこれめっちゃ失敗するんだけど（笑）
+       
+
         //DM開く
         document.dispatchEvent( new KeyboardEvent( "keypress",{keyCode:105})) ;//"i"を入力。指定の方法は色々あるようだけどこの形式でやっと反応した。
 
-        if(DMすでに出てる){
-        }else{
+        if(!DM出現判定()){            
             //DMが開き機ってないのでもっかい開く
             setTimeout(function(){
                 document.dispatchEvent( new KeyboardEvent( "keypress",{keyCode:105})) ;//"i"を入力
-            },1000);
+            },500);
         }
-        キー入力の許可=false;//これ以降の入力禁止ー！！
-    },2000);
+        setTimeout(function(){
+            キー入力の許可=false;//これ以降の入力禁止ー！！
+
+            //で、本当にDM表示できたの？
+            if(!DM出現判定()){
+                //初回のDM表示失敗することがあるから押せるボタンも用意しておこう
+                let DM_Button = document.createElement("button"); 
+                DM_Button.innerText="DM";
+                document.body.appendChild(DM_Button);
+                DM_Button.id = "DM_Button";
+                DM_Button.onclick = function(){
+                    
+                    キー入力の許可=true;//入力許可
+                    document.dispatchEvent( new KeyboardEvent( "keypress",{keyCode:105})) ;//"i"を入力
+                    //DM開く
+                    setTimeout(function(){
+                        document.dispatchEvent( new KeyboardEvent( "keypress",{keyCode:105})) ;//"i"を入力
+                    },500);
+                    
+                    setTimeout(function(){
+                        キー入力の許可=false;//これ以降の入力禁止ー！！
+
+                        //で、本当にDM表示できたの？
+                        if(DM出現判定()){
+                            DM_Button.remove();//DMでてたからボタン削除
+                        }
+                    },1000);
+                }
+            }
+        },1000);
+    },3000);
+
+
 
 });
 
